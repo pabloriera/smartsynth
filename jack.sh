be32816446c363851e0c99afc98e3868f39dd4e1
pabloriera <pablo.riera@gmail.com>
2017-05-06 13:58:08 -0300

    backup

--- jack.sh
+++ jack.sh
@@ -3,4 +3,4 @@
 ## Set the CPU scaling governor to performance
 for gov in /sys/devices/system/cpu/cpu[0-9]*/cpufreq/scaling_governor; do echo "performance" | sudo tee $gov >/dev/null; done
 
-sudo jackd --verbose -P70 -p16 -t2000 -d alsa -dhw:USB -p 256 -n 3 -r 44100 -s 
+sudo jackd --verbose -P70 -p16 -t2000 -d alsa -dhw:CODEC -p 256 -n 3 -r 44100 -s 
